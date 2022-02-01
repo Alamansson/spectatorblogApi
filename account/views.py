865c189f97bbc80fa8b593c3ee2eec31c808b7c6
Umarovabai <umarovabai8@mail.com>
2022-02-01 18:49:42 +0600

    account

--- account/views.py
+++ account/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegistrationSerializer, ActivationSerializer, LoginSerializer
+
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = RegistrationSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.create()
+            message = f'Вы успешно зарегистрированы. ' \
+                      f'Вам отправлено письмо с активацией'
+            return Response(message, status=201)
+
+class ActivationView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = ActivationSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.activate()
+            return Response('Ваш аккаунт успешно активирован')
+
+
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
